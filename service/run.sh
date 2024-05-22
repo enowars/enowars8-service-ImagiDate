bbe197ca43064ca222c1bb568d3a9923b4fadf7d
moaath <maath.oudeh@campus.tu-berlin.de>
2024-05-22 02:01:54 +0200

    modifies user permission to only select and insert data to the db

--- service/run.sh
+++ service/run.sh
@@ -5,8 +5,7 @@ while ! mysqladmin ping -h"db" --silent; do
     sleep 1
 done
 
-# init database
-mysql -h db -u date -psomepassword date < /var/www/html/init.sql
+
 
 #while true; do
 	#/service/cleanup.sh
